39a9e875e97bef42d633f666bdf2f2cf742f917a
Mustafa Ahmed <ahmed@media-it-services.de>
2025-04-20 12:55:16 +0300

    add dist

---
+++ dist/styles/ui.module.css.js
@@ -0,0 +1,326 @@
+const editorRoot = "_editorRoot_uazmk_53";
+const editorWrapper = "_editorWrapper_uazmk_154";
+const nestedListItem = "_nestedListItem_uazmk_156";
+const toolbarRoot = "_toolbarRoot_uazmk_160";
+const readOnlyToolbarRoot = "_readOnlyToolbarRoot_uazmk_187";
+const toolbarModeSwitch = "_toolbarModeSwitch_uazmk_196";
+const toolbarToggleItem = "_toolbarToggleItem_uazmk_206";
+const toolbarGroupOfGroups = "_toolbarGroupOfGroups_uazmk_217";
+const toolbarToggleSingleGroup = "_toolbarToggleSingleGroup_uazmk_222";
+const toolbarButton = "_toolbarButton_uazmk_237";
+const activeToolbarButton = "_activeToolbarButton_uazmk_274";
+const toolbarNodeKindSelectContainer = "_toolbarNodeKindSelectContainer_uazmk_284";
+const toolbarButtonDropdownContainer = "_toolbarButtonDropdownContainer_uazmk_285";
+const toolbarCodeBlockLanguageSelectContent = "_toolbarCodeBlockLanguageSelectContent_uazmk_286";
+const selectContainer = "_selectContainer_uazmk_287";
+const selectItem = "_selectItem_uazmk_300";
+const toolbarNodeKindSelectTrigger = "_toolbarNodeKindSelectTrigger_uazmk_305";
+const toolbarButtonSelectTrigger = "_toolbarButtonSelectTrigger_uazmk_306";
+const selectTrigger = "_selectTrigger_uazmk_307";
+const toolbarCodeBlockLanguageSelectTrigger = "_toolbarCodeBlockLanguageSelectTrigger_uazmk_342";
+const toolbarNodeKindSelectItem = "_toolbarNodeKindSelectItem_uazmk_347";
+const toolbarNodeKindSelectDropdownArrow = "_toolbarNodeKindSelectDropdownArrow_uazmk_372";
+const selectDropdownArrow = "_selectDropdownArrow_uazmk_373";
+const contentEditable = "_contentEditable_uazmk_379";
+const codeMirrorWrapper = "_codeMirrorWrapper_uazmk_391";
+const sandPackWrapper = "_sandPackWrapper_uazmk_400";
+const codeMirrorToolbar = "_codeMirrorToolbar_uazmk_408";
+const frontmatterWrapper = "_frontmatterWrapper_uazmk_412";
+const frontmatterToggleButton = "_frontmatterToggleButton_uazmk_422";
+const propertyPanelTitle = "_propertyPanelTitle_uazmk_430";
+const propertyEditorTable = "_propertyEditorTable_uazmk_438";
+const iconButton = "_iconButton_uazmk_456";
+const readOnlyColumnCell = "_readOnlyColumnCell_uazmk_461";
+const propertyEditorLabelCell = "_propertyEditorLabelCell_uazmk_466";
+const buttonsFooter = "_buttonsFooter_uazmk_474";
+const propertyEditorInput = "_propertyEditorInput_uazmk_480";
+const primaryButton = "_primaryButton_uazmk_506";
+const secondaryButton = "_secondaryButton_uazmk_507";
+const smallButton = "_smallButton_uazmk_522";
+const dialogForm = "_dialogForm_uazmk_534";
+const linkDialogEditForm = "_linkDialogEditForm_uazmk_540";
+const linkDialogInputContainer = "_linkDialogInputContainer_uazmk_548";
+const linkDialogInputWrapper = "_linkDialogInputWrapper_uazmk_554";
+const linkDialogInput = "_linkDialogInput_uazmk_548";
+const dialogInput = "_dialogInput_uazmk_575";
+const linkDialogAnchor = "_linkDialogAnchor_uazmk_586";
+const linkDialogPopoverContent = "_linkDialogPopoverContent_uazmk_600";
+const tableColumnEditorPopoverContent = "_tableColumnEditorPopoverContent_uazmk_601";
+const dialogContent = "_dialogContent_uazmk_602";
+const largeDialogContent = "_largeDialogContent_uazmk_614";
+const dialogTitle = "_dialogTitle_uazmk_624";
+const dialogCloseButton = "_dialogCloseButton_uazmk_630";
+const popoverContent = "_popoverContent_uazmk_637";
+const popoverArrow = "_popoverArrow_uazmk_649";
+const linkDialogPreviewAnchor = "_linkDialogPreviewAnchor_uazmk_653";
+const tooltipTrigger = "_tooltipTrigger_uazmk_676";
+const tooltipContent = "_tooltipContent_uazmk_680";
+const actionButton = "_actionButton_uazmk_694";
+const primaryActionButton = "_primaryActionButton_uazmk_701";
+const tableEditor = "_tableEditor_uazmk_713";
+const toolCell = "_toolCell_uazmk_724";
+const tableColumnEditorTrigger = "_tableColumnEditorTrigger_uazmk_743";
+const tableRowEditorTrigger = "_tableRowEditorTrigger_uazmk_744";
+const addRowButton = "_addRowButton_uazmk_745";
+const addColumnButton = "_addColumnButton_uazmk_746";
+const tableColumnEditorToolbar = "_tableColumnEditorToolbar_uazmk_789";
+const toggleGroupRoot = "_toggleGroupRoot_uazmk_803";
+const tableToolsColumn = "_tableToolsColumn_uazmk_821";
+const leftAlignedCell = "_leftAlignedCell_uazmk_830";
+const rightAlignedCell = "_rightAlignedCell_uazmk_834";
+const centeredCell = "_centeredCell_uazmk_838";
+const dialogOverlay = "_dialogOverlay_uazmk_871";
+const overlayShow = "_overlayShow_uazmk_1";
+const contentShow = "_contentShow_uazmk_1";
+const focusedImage = "_focusedImage_uazmk_918";
+const imageWrapper = "_imageWrapper_uazmk_922";
+const editImageToolbar = "_editImageToolbar_uazmk_935";
+const editImageButton = "_editImageButton_uazmk_939";
+const inlineEditor = "_inlineEditor_uazmk_945";
+const blockEditor = "_blockEditor_uazmk_954";
+const nestedEditor = "_nestedEditor_uazmk_963";
+const genericComponentName = "_genericComponentName_uazmk_982";
+const diffSourceToggle = "_diffSourceToggle_uazmk_988";
+const selectWithLabel = "_selectWithLabel_uazmk_1002";
+const toolbarTitleMode = "_toolbarTitleMode_uazmk_1017";
+const imageControlWrapperResizing = "_imageControlWrapperResizing_uazmk_1023";
+const imageResizer = "_imageResizer_uazmk_1027";
+const imageResizerN = "_imageResizerN_uazmk_1036";
+const imageResizerNe = "_imageResizerNe_uazmk_1042";
+const imageResizerE = "_imageResizerE_uazmk_1048";
+const imageResizerSe = "_imageResizerSe_uazmk_1054";
+const imageResizerS = "_imageResizerS_uazmk_1054";
+const imageResizerSw = "_imageResizerSw_uazmk_1066";
+const imageResizerW = "_imageResizerW_uazmk_1072";
+const imageResizerNw = "_imageResizerNw_uazmk_1078";
+const placeholder = "_placeholder_uazmk_1084";
+const rootContentEditableWrapper = "_rootContentEditableWrapper_uazmk_1097";
+const downshiftContainer = "_downshiftContainer_uazmk_1103";
+const downshiftInputWrapper = "_downshiftInputWrapper_uazmk_1109";
+const downshiftInput = "_downshiftInput_uazmk_1109";
+const downshiftAutocompleteContainer = "_downshiftAutocompleteContainer_uazmk_1140";
+const textInput = "_textInput_uazmk_1186";
+const multiFieldForm = "_multiFieldForm_uazmk_1194";
+const formField = "_formField_uazmk_1200";
+const markdownParseError = "_markdownParseError_uazmk_1211";
+const popupContainer = "_popupContainer_uazmk_1220";
+const inputSizer = "_inputSizer_uazmk_1225";
+const styles = {
+  editorRoot,
+  editorWrapper,
+  nestedListItem,
+  toolbarRoot,
+  readOnlyToolbarRoot,
+  toolbarModeSwitch,
+  toolbarToggleItem,
+  toolbarGroupOfGroups,
+  toolbarToggleSingleGroup,
+  toolbarButton,
+  activeToolbarButton,
+  toolbarNodeKindSelectContainer,
+  toolbarButtonDropdownContainer,
+  toolbarCodeBlockLanguageSelectContent,
+  selectContainer,
+  selectItem,
+  toolbarNodeKindSelectTrigger,
+  toolbarButtonSelectTrigger,
+  selectTrigger,
+  toolbarCodeBlockLanguageSelectTrigger,
+  toolbarNodeKindSelectItem,
+  toolbarNodeKindSelectDropdownArrow,
+  selectDropdownArrow,
+  contentEditable,
+  codeMirrorWrapper,
+  sandPackWrapper,
+  codeMirrorToolbar,
+  frontmatterWrapper,
+  frontmatterToggleButton,
+  propertyPanelTitle,
+  propertyEditorTable,
+  iconButton,
+  readOnlyColumnCell,
+  propertyEditorLabelCell,
+  buttonsFooter,
+  propertyEditorInput,
+  primaryButton,
+  secondaryButton,
+  smallButton,
+  dialogForm,
+  linkDialogEditForm,
+  linkDialogInputContainer,
+  linkDialogInputWrapper,
+  linkDialogInput,
+  dialogInput,
+  linkDialogAnchor,
+  linkDialogPopoverContent,
+  tableColumnEditorPopoverContent,
+  dialogContent,
+  largeDialogContent,
+  dialogTitle,
+  dialogCloseButton,
+  popoverContent,
+  popoverArrow,
+  linkDialogPreviewAnchor,
+  tooltipTrigger,
+  tooltipContent,
+  actionButton,
+  primaryActionButton,
+  tableEditor,
+  toolCell,
+  tableColumnEditorTrigger,
+  tableRowEditorTrigger,
+  addRowButton,
+  addColumnButton,
+  tableColumnEditorToolbar,
+  toggleGroupRoot,
+  tableToolsColumn,
+  leftAlignedCell,
+  rightAlignedCell,
+  centeredCell,
+  dialogOverlay,
+  overlayShow,
+  contentShow,
+  focusedImage,
+  imageWrapper,
+  editImageToolbar,
+  editImageButton,
+  inlineEditor,
+  blockEditor,
+  nestedEditor,
+  genericComponentName,
+  diffSourceToggle,
+  selectWithLabel,
+  toolbarTitleMode,
+  imageControlWrapperResizing,
+  imageResizer,
+  imageResizerN,
+  imageResizerNe,
+  imageResizerE,
+  imageResizerSe,
+  imageResizerS,
+  imageResizerSw,
+  imageResizerW,
+  imageResizerNw,
+  placeholder,
+  rootContentEditableWrapper,
+  downshiftContainer,
+  downshiftInputWrapper,
+  downshiftInput,
+  downshiftAutocompleteContainer,
+  textInput,
+  multiFieldForm,
+  formField,
+  markdownParseError,
+  popupContainer,
+  inputSizer
+};
+export {
+  actionButton,
+  activeToolbarButton,
+  addColumnButton,
+  addRowButton,
+  blockEditor,
+  buttonsFooter,
+  centeredCell,
+  codeMirrorToolbar,
+  codeMirrorWrapper,
+  contentEditable,
+  contentShow,
+  styles as default,
+  dialogCloseButton,
+  dialogContent,
+  dialogForm,
+  dialogInput,
+  dialogOverlay,
+  dialogTitle,
+  diffSourceToggle,
+  downshiftAutocompleteContainer,
+  downshiftContainer,
+  downshiftInput,
+  downshiftInputWrapper,
+  editImageButton,
+  editImageToolbar,
+  editorRoot,
+  editorWrapper,
+  focusedImage,
+  formField,
+  frontmatterToggleButton,
+  frontmatterWrapper,
+  genericComponentName,
+  iconButton,
+  imageControlWrapperResizing,
+  imageResizer,
+  imageResizerE,
+  imageResizerN,
+  imageResizerNe,
+  imageResizerNw,
+  imageResizerS,
+  imageResizerSe,
+  imageResizerSw,
+  imageResizerW,
+  imageWrapper,
+  inlineEditor,
+  inputSizer,
+  largeDialogContent,
+  leftAlignedCell,
+  linkDialogAnchor,
+  linkDialogEditForm,
+  linkDialogInput,
+  linkDialogInputContainer,
+  linkDialogInputWrapper,
+  linkDialogPopoverContent,
+  linkDialogPreviewAnchor,
+  markdownParseError,
+  multiFieldForm,
+  nestedEditor,
+  nestedListItem,
+  overlayShow,
+  placeholder,
+  popoverArrow,
+  popoverContent,
+  popupContainer,
+  primaryActionButton,
+  primaryButton,
+  propertyEditorInput,
+  propertyEditorLabelCell,
+  propertyEditorTable,
+  propertyPanelTitle,
+  readOnlyColumnCell,
+  readOnlyToolbarRoot,
+  rightAlignedCell,
+  rootContentEditableWrapper,
+  sandPackWrapper,
+  secondaryButton,
+  selectContainer,
+  selectDropdownArrow,
+  selectItem,
+  selectTrigger,
+  selectWithLabel,
+  smallButton,
+  tableColumnEditorPopoverContent,
+  tableColumnEditorToolbar,
+  tableColumnEditorTrigger,
+  tableEditor,
+  tableRowEditorTrigger,
+  tableToolsColumn,
+  textInput,
+  toggleGroupRoot,
+  toolCell,
+  toolbarButton,
+  toolbarButtonDropdownContainer,
+  toolbarButtonSelectTrigger,
+  toolbarCodeBlockLanguageSelectContent,
+  toolbarCodeBlockLanguageSelectTrigger,
+  toolbarGroupOfGroups,
+  toolbarModeSwitch,
+  toolbarNodeKindSelectContainer,
+  toolbarNodeKindSelectDropdownArrow,
+  toolbarNodeKindSelectItem,
+  toolbarNodeKindSelectTrigger,
+  toolbarRoot,
+  toolbarTitleMode,
+  toolbarToggleItem,
+  toolbarToggleSingleGroup,
+  tooltipContent,
+  tooltipTrigger
+};
